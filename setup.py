fa10e5efe8fdda0d7bb10be591bd62117d09073a
Knight, Eddie <iv.eddieknight@gmail.com>
2020-04-14 13:35:14 -0400

    Updated setup for pypi

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
-        "License :: CCNC License",
+        "LICENSE :: FREE FOR NON-COMMERCIAL USE",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
