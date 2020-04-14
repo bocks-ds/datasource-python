cf5a71ab0a5b7f53eabd04faf3c691eea2d539db
Knight, Eddie <iv.eddieknight@gmail.com>
2020-04-14 14:16:40 -0400

    Specifying version numbers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bocks_ds",
-    version="0.0.1",
+    version="0.2.1",
     author="Bocks",
     description="DataSource Python SDK",
     long_description_content_type="text/markdown",
